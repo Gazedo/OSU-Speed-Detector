994aa73d042a542e6ead58ce5f4c1dd999f7cdc1
Hayden Connor <connorh@oregonstate.edu>
2016-09-02 00:20:11 -0700

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import kivy
+kivy.require('1.9.1')
+
+# Show live preview of camera
+# When button is pushed, start saving to local storage video, detected distance, and raw imu measurements
+
+# OpenCV:
+# Record numbers from images
+
+# Camera:
+# Live display
+# Stream to file
+# Save to disk
+
+# GUI Settings backend
+# Swipe down for settings
+# ?pull sideways for brightness
+
+from kivy.app import App
+class Main(App):
+    pass
+
+if __name__ == '__main__':
+    Main().run()
